091e8754b6d4ae7990eb4604d10e5a39b67accf5
sanyath007 <sanyath007@gmail.com>
2021-04-07 11:17:01 +0700

    Edit getProductWard, getProductAdd and getSummary of ProductivityController by modifying where clause of ward response data with not in 03,04,13,14,15 and 17

--- src/Controllers/ProductivityController.php
+++ src/Controllers/ProductivityController.php
@@ -23,7 +23,7 @@ class ProductivityController extends Controller
 
         return $res->withJson([
             'product' => DB::connection('pharma')->select($sql, [$sdate, $edate, $args['ward']]),
-            'wards' => Ward::whereNotIn('ward', ['03','04','05'])->get(),
+            'wards' => Ward::whereNotIn('ward', ['03','04','13','14','15','17'])->get(),
         ]);
     }
 
@@ -74,7 +74,7 @@ class ProductivityController extends Controller
 
     public function getProductAdd($req, $res, $args)
     {
-        return $res->withJson(Ward::whereNotIn('ward', ['03','04','05'])->get());
+        return $res->withJson(Ward::whereNotIn('ward', ['03','04','13','14','15','17'])->get());
     }
 
     public function getWorkload($req, $res, $args)
@@ -132,7 +132,7 @@ class ProductivityController extends Controller
                 order by ward, period ";
         
         $wards = [];
-        $ws = Ward::whereNotIn('ward', ['03','04','05'])->orderBy('ward')->get(['ward', 'name']);
+        $ws = Ward::whereNotIn('ward', ['03','04','13','14','15','17'])->orderBy('ward')->get(['ward', 'name']);
         foreach ($ws as $key => $value) {
             for($p = 1; $p <= 3; $p++){
                 array_push($wards, [
