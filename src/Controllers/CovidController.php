e382e3ffc440c63a1f44923419544db3dd08d4ae
sanyath007 <sanyath007@gmail.com>
2021-08-03 14:43:02 +0700

    Edit querying data from db of all method of CovidController by adding case when an in icd10='B342'

--- src/Controllers/CovidController.php
+++ src/Controllers/CovidController.php
@@ -16,6 +16,7 @@ class CovidController extends Controller
                 LEFT JOIN thaiaddress t ON (t.addressid=concat(p.chwpart, p.amppart, p.tmbpart))
                 WHERE (i.ward IN ('00', '06', '10', '11', '12'))
                 AND (i.dchdate is null)
+                AND (i.an in (select an from iptdiag where icd10='B342'))
                 AND (p.chwpart='30' AND p.amppart='01')
                 GROUP BY t.addressid, t.name
                 ORDER BY count(i.an) DESC";
@@ -25,14 +26,15 @@ class CovidController extends Controller
     
     public function getPatientsTambon($req, $res, $args)
     {
-        $sql="SELECT i.an, i.hn, i.regdate, i.regtime, concat(i.ward, '-', w.name) as ward,
-                concat(p.pname, p.fname, ' ', p.lname) as ptname,
-                t.full_name as address, p.addrpart, p.moopart
+        $sql="SELECT i.an, i.hn, i.regdate, i.regtime, concat(i.ward, '-', w.name) AS ward,
+                concat(p.pname, p.fname, ' ', p.lname) AS ptname,
+                t.full_name AS address, p.addrpart, p.moopart
                 FROM ipt i 
-                LEFT JOIN patient p on (p.hn=i.hn)
-                LEFT JOIN ward w on (i.ward=w.ward)
-                LEFT JOIN thaiaddress t on (t.addressid=concat(p.chwpart, p.amppart, p.tmbpart))
-                WHERE (i.ward in ('00', '06', '10', '11', '12'))
+                LEFT JOIN patient p ON (p.hn=i.hn)
+                LEFT JOIN ward w ON (i.ward=w.ward)
+                LEFT JOIN thaiaddress t ON (t.addressid=concat(p.chwpart, p.amppart, p.tmbpart))
+                WHERE (i.ward IN ('00', '06', '10', '11', '12'))
+                AND (i.an IN (select an from iptdiag where icd10='B342'))
                 AND (i.dchdate is null)
                 AND (t.addressid=?)";
 
@@ -44,26 +46,28 @@ class CovidController extends Controller
 
     public function getNumBed($req, $res, $args)
     {
-        $sql="SELECT i.ward, w.name, count(i.an) as num_pt
+        $sql="SELECT i.ward, w.name, 
+                COUNT(CASE WHEN (i.an IN (select an from iptdiag where icd10='B342')) THEN i.an END) AS num_pt
                 FROM ipt i 
                 LEFT JOIN ward w ON (i.ward=w.ward)
                 WHERE (i.ward IN ('00', '06', '10', '11', '12'))
                 AND (i.dchdate is null)
                 GROUP BY i.ward, w.name
-                ORDER BY count(i.an) DESC";
+                ORDER BY COUNT(i.an) DESC";
 
         return $res->withJson(DB::select($sql));
     }
 
     public function getCardStat($req, $res, $args)
     {
-        $sql="SELECT DATE(NOW()) as today, 
+        $sql="SELECT DATE(NOW()) AS today, 
                 COUNT(CASE WHEN (i.regdate = DATE(NOW())) THEN i.an END) AS new_case,
-                COUNT(CASE WHEN (i.regdate < DATE(NOW()) AND i.dchdate is null) THEN i.an end) AS top_case,
+                COUNT(CASE WHEN (i.regdate < DATE(NOW()) AND i.dchdate is null) THEN i.an END) AS top_case,
                 COUNT(CASE WHEN (i.dchdate = DATE(NOW())) THEN i.an END) AS discharge,
                 COUNT(CASE WHEN (i.dchdate is null) THEN i.an END) AS still
                 FROM ipt i LEFT JOIN ward w ON (i.ward=w.ward)
-                WHERE (i.ward IN ('00', '06', '10', '11', '12')) ";
+                WHERE (i.ward IN ('00', '06', '10', '11', '12'))
+                AND (i.an in (select an from iptdiag where icd10='B342')) ";
 
         return $res->withJson(collect(DB::select($sql))->first());
     }
